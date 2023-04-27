6b2a88480f58fd8a496c529265e2b550ce6cb59e
Kailash Sharma <kailashps.1011@gmail.com>
2023-04-28 01:03:30 +0530

    Adding changes to build.gradle.kts again

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     kotlin("jvm") version "1.8.0"
     id("io.ktor.plugin") version "2.2.2"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 group = "com.sockets.web"
